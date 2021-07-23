c13501fd7eb58fe2a4257ad00903713232632f0b
Juan Carlos Salvador <salvaju@oocl.com>
2021-07-23 14:44:46 +0800

    remove comments

--- 2.function2/main.js
+++ 2.function2/main.js
@@ -18,24 +18,3 @@ function palindrome(message) {
 
 palindrome('hello');
 palindrome('abcba');
-
-
-// function reverseString(message) {
-
-//     var reversedStrArr = [];
-//     var reversedStr = '';
-
-//     for(i =  message.length - 1; i >= 0; i--) {
-//         reversedStrArr.push(message[i]);
-//     }
-
-//     for(i = 0; i <= reversedStrArr.length - 1; i++) {
-//         reversedStr = reversedStr + reversedStrArr[i];
-//     }
-
-//     console.log(reversedStr);
-//     return reversedStr;
-
-// }
-
-// reverseString('hello');
